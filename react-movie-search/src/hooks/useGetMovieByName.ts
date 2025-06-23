9362ad5f45aed8fd426ea77ed0e77faacc8266d7
Jose Sissa <sissamendoza@outlook.com>
2025-06-23 14:33:49 -0500

    useCallback

--- react-movie-search/src/hooks/useGetMovieByName.ts
+++ react-movie-search/src/hooks/useGetMovieByName.ts
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react'
+import { useMemo, useRef, useState, useCallback } from 'react'
 import type { MovieData } from '../types/types';
 import { SearchMovies } from '../services/movies';
 
@@ -7,19 +7,18 @@ export function useGetMovieByName({ search, sort }: { search: string, sort: bool
     const [loading, setLoading] = useState(false)
     const prevSearch = useRef(search)
 
-    const getDataMovie = useMemo(() => {
-        return async ({ search }: { search: string }) => {
-            if (search === prevSearch.current) return
-            try {
-                setLoading(true)
-                prevSearch.current = search
-                const listMovies = await SearchMovies(search)
-                setMovieData(listMovies)
-            } catch (error: any) {
-                console.log(error.message);
-            } finally {
-                setLoading(false)
-            }
+    // useCallback is the same that useMemo but with best sintax
+    const getDataMovie = useCallback(async ({ search }: { search: string }) => {
+        if (search === prevSearch.current) return
+        try {
+            setLoading(true)
+            prevSearch.current = search
+            const listMovies = await SearchMovies(search)
+            setMovieData(listMovies)
+        } catch (error: any) {
+            console.log(error.message);
+        } finally {
+            setLoading(false)
         }
     }, [])
 
