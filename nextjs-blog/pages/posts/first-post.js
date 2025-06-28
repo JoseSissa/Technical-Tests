db16f04b15c0a80fb0c72778b02cb1cf45cd9fe8
Jose Sissa <sissamendoza@outlook.com>
2025-06-28 11:36:40 -0500

    script tag

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,21 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <Layout>
             <Head>
                 <title>First Post</title>
+                {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
             </Head>
             <h1>First Post</h1>
             <h2>
