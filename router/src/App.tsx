6b374e34eebd04e09fffa13bc69681a53c49943a
Jose Sissa <sissamendoza@outlook.com>
2025-07-01 10:47:27 -0500

    First indications created

--- router/src/App.tsx
+++ router/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import { EVENT } from './config/const'
 
 export function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>This is the home page</p>
-      <a href="/about">About</a>
+      <button onClick={() => navigate('/about')}>About</button>
     </>
   )
 }
@@ -15,17 +17,42 @@ export function AboutPage() {
     <>
       <h1>About</h1>
       <p>This is the about page</p>
-      <a href="/">Home</a>
+      <button onClick={() => navigate('/')}>Home</button>
     </>
   )
 }
 
+function navigate(href: string) {
+  // Change the URL
+  window.history.pushState({}, '', href)
+  // Creeate a new event
+  const navigation = new Event(EVENT.PUSHSTATE)
+  // Send the event
+  window.dispatchEvent(navigation)
+}
+
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+    // Listener for next navigation
+    window.addEventListener(EVENT.PUSHSTATE, onLocationChange)
+    // Listener for back navigation
+    window.addEventListener(EVENT.POPSTATE, onLocationChange)
+    // Cleanup function to avoid memory leaks
+    return () => {
+      window.removeEventListener(EVENT.PUSHSTATE, onLocationChange)
+      window.removeEventListener(EVENT.POPSTATE, onLocationChange)
+    }
+  }, [])
 
   return (
     <main>
-      <HomePage />
-      <AboutPage />
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
     </main>
   )
 }
