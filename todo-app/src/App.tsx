aa9ed8d0006f0f86ada46c34521db87b4dc1ad89
Jose Sissa <sissamendoza@outlook.com>
2025-07-03 16:12:31 -0500

    delete all completed todo added

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -8,34 +8,46 @@ import { TODO_FILTERS } from './utils/const'
 
 function App() {
   const [todos, setTodos] = useState(mockTodos)
+  const [filteredTodos, setFilteredTodos] = useState(mockTodos)
 
   const handleRemove = (id: TodoID) => {
     const updatedTodos = todos.filter((todo) => todo.id !== id)
+    setFilteredTodos(updatedTodos)
     setTodos(updatedTodos)
   }
 
   const handleCompletedTodo = (id: TodoID) => {
     const updatedTodos = todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
     setTodos(updatedTodos)
+    setFilteredTodos(updatedTodos)
   }
 
   const handleFilter = (filter: typeof TODO_FILTERS[keyof typeof TODO_FILTERS]) => {
     if (filter === TODO_FILTERS.ALL) {
-      setTodos(mockTodos)
+      setFilteredTodos(todos)
     }
     else if (filter === TODO_FILTERS.ACTIVE) {
-      setTodos(mockTodos.filter((todo) => todo.completed === false))
+      setFilteredTodos(todos.filter((todo) => todo.completed === false))
     }
     else if (filter === TODO_FILTERS.COMPLETED) {
-      setTodos(mockTodos.filter((todo) => todo.completed === true))
+      setFilteredTodos(todos.filter((todo) => todo.completed === true))
+    }
+  }
+
+  const deleteTodoCompleted = () => {
+    const result = confirm('Do you want to delete all completed todos?')
+    if (result) {
+      const updatedTodos = todos.filter((todo) => todo.completed === false)
+      setTodos(updatedTodos)
+      setFilteredTodos(updatedTodos)
     }
   }
 
   return (
     <>
       <h1>todo mvc</h1>
-      <Todos todos={todos} handleRemove={handleRemove} handleCompletedTodo={handleCompletedTodo} />
-      <Footer handleFilter={handleFilter} />
+      <Todos todos={filteredTodos} handleRemove={handleRemove} handleCompletedTodo={handleCompletedTodo} />
+      <Footer handleFilter={handleFilter} deleteTodoCompleted={deleteTodoCompleted} />
     </>
   )
 }
--- todo-app/src/components/Footer.tsx
+++ todo-app/src/components/Footer.tsx
@@ -2,15 +2,17 @@ import { TODO_FILTERS } from '../utils/const'
 
 interface Props {
     handleFilter: (filter: typeof TODO_FILTERS[keyof typeof TODO_FILTERS]) => void
+    deleteTodoCompleted: () => void
 }
 
-export const Footer: React.FC<Props> = ({ handleFilter }) => {
+export const Footer: React.FC<Props> = ({ handleFilter, deleteTodoCompleted }) => {
     return (
         <footer>
             <div>
                 <button onClick={() => handleFilter(TODO_FILTERS.ALL)}>{TODO_FILTERS.ALL}</button>
                 <button onClick={() => handleFilter(TODO_FILTERS.ACTIVE)}>{TODO_FILTERS.ACTIVE}</button>
                 <button onClick={() => handleFilter(TODO_FILTERS.COMPLETED)}>{TODO_FILTERS.COMPLETED}</button>
+                <button onClick={() => deleteTodoCompleted()}>Clear completed</button>
             </div>
         </footer>
     )
